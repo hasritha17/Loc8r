e6a45ac7f5a41c728a382f1832a30aa9cd60e8eb
hasritha17 <21eg106a11@anurag.edu.in>
2023-09-25 20:26:22 +0530

    First Commit

---
+++ app_server/routes/index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+var express = require('express');
+var router = express.Router();
+var ctrlLocations = require('../controllers/locations.js')
+var ctrlOthers = require('../controllers/others.js')
+
+router.get('/', ctrlLocations.homelist);
+router.get('/location',ctrlLocations.locationInfo);
+router.get('/location2',ctrlLocations.locationInfo2);
+router.get('/location3',ctrlLocations.locationInfo3);
+
+router.get('/location/review/new',ctrlLocations.addReview);
+router.get('/location/review/new2',ctrlLocations.addReview2);
+router.get('/location/review/new3',ctrlLocations.addReview3);
+
+router.get('/about',ctrlOthers.about);
+
+module.exports = router;
